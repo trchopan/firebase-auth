99f02ffcd18aaaba29ae5cf77474c9fa0a874111
Chop TRAN <m@chop.dev>
2023-11-01 15:59:51 +0700

    Update add usage with JSON extractor

--- examples/axum_sqlite.rs
+++ examples/axum_sqlite.rs
@@ -1,4 +1,7 @@
+use std::env;
+
 use axum::{
+    extract,
     response::IntoResponse,
     routing::{get, post},
     Extension, Json, Router,
@@ -8,53 +11,51 @@ use env_logger::Env;
 use firebase_auth::{FirebaseAuth, FirebaseAuthState, FirebaseUser};
 use reqwest::StatusCode;
 use serde::{Deserialize, Serialize};
-use sqlx::{migrate::MigrateDatabase, Sqlite, SqlitePool};
+use sqlx::SqlitePool;
 
 #[derive(sqlx::FromRow, Deserialize, Serialize)]
 pub struct Visit {
     pub id: i64,
     pub user_id: String,
     pub timestamp: String,
+    pub detail: String,
 }
 
 async fn public() -> &'static str {
     "ok"
 }
 
-const DB_URL: &str = "sqlite://sqlite.db";
+const DB_URL: &str = "sqlite::memory:";
 
 async fn init_database() -> SqlitePool {
-    if !Sqlite::database_exists(DB_URL).await.unwrap_or(false) {
-        println!("Creating database {}", DB_URL);
-        match Sqlite::create_database(DB_URL).await {
-            Ok(_) => println!("Create db success"),
-            Err(error) => panic!("error: {}", error),
-        }
-    } else {
-        println!("Database already exists");
-    }
-
     let db = SqlitePool::connect(DB_URL).await.unwrap();
     let result = sqlx::query(
-        "CREATE TABLE IF NOT EXISTS visit (id INTEGER PRIMARY KEY NOT NULL, user_id VARCHAR(32), timestamp VARCHAR(250) NOT NULL);"
+        "CREATE TABLE IF NOT EXISTS visit (id INTEGER PRIMARY KEY NOT NULL, user_id VARCHAR(32), timestamp VARCHAR(25), detail VARCHAR(255) NOT NULL);"
     ).execute(&db).await.unwrap();
     println!("Create visit table result: {:?}", result);
 
     db
 }
 
+#[derive(Deserialize)]
+pub struct NewVisit {
+    pub detail: String,
+}
+
 pub async fn new_visit(
     user: FirebaseUser,
     Extension(pool): Extension<SqlitePool>,
+    extract::Json(visit): extract::Json<NewVisit>,
 ) -> impl IntoResponse {
     let user_id = user.user_id;
     let timestamp = Utc::now().to_string();
 
-    let sql = "INSERT INTO visit (user_id, timestamp) values ($1, $2) RETURNING *";
+    let sql = "INSERT INTO visit (user_id, timestamp, detail) values ($1, $2, $3) RETURNING *";
 
     let result: Result<Visit, sqlx::Error> = sqlx::query_as(sql)
         .bind(user_id.clone())
         .bind(timestamp.clone())
+        .bind(visit.detail.clone())
         .fetch_one(&pool)
         .await;
 
@@ -68,6 +69,7 @@ pub async fn new_visit(
                     id: 0,
                     user_id,
                     timestamp,
+                    detail: visit.detail,
                 }),
             )
         }
@@ -80,7 +82,7 @@ pub async fn my_visits(
 ) -> impl IntoResponse {
     let user_id = user.user_id;
 
-    let sql = "SELECT id, user_id, timestamp FROM visit WHERE user_id=$1".to_string();
+    let sql = "SELECT id, user_id, timestamp, detail FROM visit WHERE user_id=$1".to_string();
 
     let result: Result<Vec<Visit>, sqlx::Error> = sqlx::query_as::<_, Visit>(&sql)
         .bind(user_id)
@@ -102,7 +104,8 @@ async fn main() {
 
     let pool = init_database().await;
 
-    let firebase_auth = FirebaseAuth::new("my-project-id").await;
+    let project_id = env::var("PROJECT_ID").unwrap_or_else(|_| panic!("must set PROJECT_ID"));
+    let firebase_auth = FirebaseAuth::new(&project_id).await;
 
     let app = Router::new()
         .route("/", get(public))
