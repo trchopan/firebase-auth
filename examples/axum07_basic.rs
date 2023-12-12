0f44b54a78314aa3782d82f48150ef075f058516
Artem Pechenyi <pechkinator@gmail.com>
2023-12-12 21:30:48 +0200

    Support for Axum v0.7 via  feature

---
+++ examples/axum07_basic.rs
@@ -0,0 +1,28 @@
+use axum07 as axum;
+
+use axum::{routing::get, Router};
+use firebase_auth::{FirebaseAuth, FirebaseAuthState, FirebaseUser};
+
+async fn greet(user: FirebaseUser) -> String {
+    let email = user.email.unwrap_or("empty email".to_string());
+    format!("hello {}", email)
+}
+
+async fn public() -> &'static str {
+    "ok"
+}
+
+#[tokio::main]
+async fn main() {
+    let firebase_auth = FirebaseAuth::new("my-project-id").await;
+
+    let app = Router::new()
+        .route("/hello", get(greet))
+        .route("/", get(public))
+        .with_state(FirebaseAuthState { firebase_auth });
+
+    let addr = "127.0.0.1:8080";
+    let listener = tokio::net::TcpListener::bind(addr).await.unwrap();
+
+    axum::serve(listener, app).await.unwrap();
+}
